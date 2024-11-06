61d6c8286f7469cafa4d41f20f9184fce61db6d6
yangxuan <yangxuan09@100.me>
2024-11-06 15:29:04 +0800

    feat: 增加二维码

--- index.d.ts
+++ index.d.ts
@@ -14,3 +14,18 @@ interface RouterItemType {
   isMenu?: boolean;
   // children?: RouterItemType[];
 }
+
+declare module "*.jpg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.jpeg" {
+  const value: string;
+  export default value;
+}
--- src/pages/ContractSetUp/components/SingleCustomer.tsx
+++ src/pages/ContractSetUp/components/SingleCustomer.tsx
@@ -1,9 +1,11 @@
 import { SingleCustomerType } from "@/types/fetchResponse";
 import style from "../styles/singleCustomer.module.css";
-import { DeleteOutlined, EditOutlined, CopyOutlined } from "@ant-design/icons";
-import { memo } from "react";
+import { QrcodeOutlined, SettingOutlined } from "@ant-design/icons";
+import { memo, useCallback, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { PAGE_PATH } from "@/types/common";
+import { Modal, QRCode } from "antd";
+import qrlogo from "@/common/statics/imgs/qrlogo.jpg";
 
 interface SingleCustomerPropsType {
   customer: SingleCustomerType;
@@ -14,17 +16,37 @@ const SingleCustomer: React.FC<SingleCustomerPropsType> = (props) => {
   const jumpToClassSettingPage = () => {
     navigate(`/page/${PAGE_PATH.CLASS_SETUP}/${customer.id}`, { replace: true });
   };
+  const [modalVisible, setModalVisible] = useState(false);
+  const modalHandle = useCallback((e: any) => {
+    e.stopPropagation(); // 阻止事件冒泡
+    setModalVisible(!modalVisible)
+  }, [modalVisible])
   return (
     <div className={style.container} onClick={jumpToClassSettingPage}>
+      <Modal
+        title={customer.name}
+        destroyOnClose
+        open={modalVisible}
+        footer={null}
+        onCancel={modalHandle}
+      >
+        <div className={style.qrCodeBox}>  
+          <QRCode 
+            errorLevel="H"
+            size={200}
+            value={`https://www.onelight.ink/ql/user2c?templateId=${customer.templateId}&customerId=${customer.id}`}
+            icon={qrlogo}
+          />
+        </div>
+      </Modal>
       <div className={style.contentContainer}>
         <div className={style.infoContainer}>
           <div className={style.titleText}>{customer.name}</div>
           <div className={style.descText}>{customer.desc}</div>
         </div>
         <div className={style.handleBar}>
-          <DeleteOutlined className={style.handleBtn} />
-          <EditOutlined className={style.handleBtn} />
-          <CopyOutlined className={style.handleBtn} />
+          <SettingOutlined className={style.handleBtn} />
+          <QrcodeOutlined className={style.handleBtn} onClick={modalHandle}/>
         </div>
       </div>
     </div>
